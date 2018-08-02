9a4a7581262855984434cbac5ba60d1b9d183f5b
Arpit Meena <arpitmeena@walkover.in>
2018-08-02 10:38:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+export interface IUser {
+  name: string;
+  email: string;
+  mobile: string;
+  gender: string;
+  city: string;
+  hobbies: string[];
+  address: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  public userStream$: Observable<IUser[]>;
+
+  public myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.createUserForm();
+  }
+
+  ngOnInit() {
+    this.userStream$ = Observable.of([{
+      name: 'Arpit',
+      email: 'abc@gmail.com',
+      mobile: '1234567890',
+      gender: 'M',
+      city: 'Indore',
+      hobbies: ['game'],
+      address: 'Indore'
+    }]);
+  }
+
+  private createUserForm() {
+    return this.fb.group({
+      name: ['', Validators.required],
+      email: '',
+      mobile: '',
+      gender: '',
+      city: '',
+      hobbies: '',
+      address: '',
+    });
+  }
+
+  public onSubmit(data) {
+    console.log('the form data is :', data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AlertModule } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '../../node_modules/@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
